5c8fd854e1f90d7a76ac2e29271e5ff5f66163ea
KamleshPatel98 <kamleshp52170@gmail.com>
2024-03-25 23:37:18 +0530

    change the input design

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,4 +1,9 @@
 <div class="mt-3">
-    <label class="form-label">{{ $label }}</label>
-    <input type="{{ $type }}" class="form-control" name="{{ $name }}">
+    {{-- <label class="form-label">{{ $label }}</label>--}}
+    <div class="row">
+        <div class="col-md-4">{{ $label }}</div>
+        <div class="col-md-8">
+            <input type="{{ $type }}" class="form-control" name="{{ $name }}"> 
+        </div>
+    </div>
 </div>
